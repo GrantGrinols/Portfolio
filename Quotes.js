2ae161af6ed3bfc88103e30593191e7dd8fc88c6
GrantGrinols <grantgrinols@oakland.edu>
2022-03-31 14:34:33 -0400

    Quotes

---
+++ Quotes.js
@@ -0,0 +1,10 @@
+ArrayofQuotes=["All our dreams can come true if we have the courage to pursue them - Walt Disney",
+"Failure is the opportunity to begin again more intelligently - Henry Ford",
+"Every experience I have is perfect for my own growth - Louise Hay",
+"Nothing is more beautiful than honesty - Steven Aitchison",
+"Don't cry because it's over; smile because it happened - Dr. Seuss"];
+function FetchQuote(){
+    document.getElementById('QuoteoftheDay').innerText = "I'm king of the world!";
+}
+console.log("Hello")
+FetchQuote();
